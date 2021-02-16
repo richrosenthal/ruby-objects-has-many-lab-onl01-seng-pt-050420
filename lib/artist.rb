d3b925a8395c1496ea1123a3cb501bc66b38df5b
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2021-02-16 02:03:09 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,18 @@
 class Artist
   attr_accessor :name :songs 
 
+  @@songs = []
+
   def initialize(name)
     @name = name
   end
 
  def add_song(song)
+   @@songs << song 
  end
 
  def add_song_by_name(name)
+   @@songs << song 
  end
 
  def song_count
